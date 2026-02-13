037731c2df6579844b068329acaf9a139b2a20ee
johnluisdramayo12-design <johnluisdramayo12@gmail.com>
2026-02-13 15:10:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ noBtn.addEventListener("touchstart", (e) => {
 // --------------------
 
 const slides = [
-    { img: "images/photo1.jpg", text: "Our First Meet - May 30-31, 2025\nTotal Hours Spent: 33hrs" },
+    { img: "images/photo1.jpg", text: "Our First Meet - May 30-31, 2025          Total Hours Spent: 33hrs" },
     { img: "images/photo2.jpg", text: "Your Birthday Celebration - July 17-18, 2025" },
     { img: "images/photo3.jpg", text: "My first time seeing your Nanay - November 22-23, 2025" },
     { img: "images/photo4.jpg", text: "My Birthday Celebration and the night that you said yes - December 12-14, 2025 – Feb 17–18" },
@@ -155,6 +155,7 @@ musicBtn.addEventListener("click", () => {
         musicBtn.textContent = "Music OFF 🔇";
     }
 });
+
 
 
 
